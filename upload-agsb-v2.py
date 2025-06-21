db97682f0965ea74572bd76274279316343a7d21
lenlau3000 <lenlau3000@gmail.com>
2025-06-21 23:05:33 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+USER_NAME = "lenlau300"         # 用户名
+UUID = "25bef35f-4c0f-4fb7-a3f4-95c1cd62f7b5"                     # UUID，留空则自动生成
+PORT = 27099                   # Vmess端口，留空或0则自动生成
 DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiNmZkYzhjOGE4NWUzYWQ1YjYxMzM5OWI3ZWMzN2U5YjIiLCJ0IjoiNTQ4ZjVhZGMtM2VmZi00MzMzLWI1YTUtNmIxM2E3ZDI3NGVmIiwicyI6Ik9EUmpPVFF5WldJdFlXRm1ZeTAwTTJFMExUZzFNak10TkRSak5HSmtOV1ZoWWpReSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
